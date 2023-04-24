5f2885ade1323deb328d3b9795cf15e18a9801dd
AniTovmasyan <118201853+AniTovmasyan@users.noreply.github.com>
2023-04-24 21:10:10 +0400

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,25 @@
+function bigImg(x) {
+  x.style.height = "90%";
+  x.style.width = "90%";
+}
+
+function normalImg(x) {
+  x.style.height = "80%";
+  x.style.width = "80%";
+}
+
+function mouseOver0() {
+  document.getElementById("demo0").style.color = "blue";
+}
+
+function mouseOut0() {
+  document.getElementById("demo0").style.color = "black";
+}
+
+function changeFontOn(x) {
+  x.style.fontSize = "30px";
+}
+
+function changeFontOff(x) {
+  x.style.fontSize = "25px";
+}
